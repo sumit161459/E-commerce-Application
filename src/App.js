2be61a5545570ff466e31310aa4550432eb173c9
Sumit Raj <toppersumitr@iitkgp.ac.in>
2021-07-06 10:50:20 +0530

    adding payement gateway and final confirmation page

--- src/App.js
+++ src/App.js
@@ -1,104 +1,95 @@
-import React,{useState,useEffect} from 'react';
-import {commerce} from './lib/commerce';
-import {Products,Navbar,Cart,Checkout} from './components';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-    const [products,setProducts]=useState([]);
-    const [cart,setCart]=useState({});
-    const [order, setOrder] = useState({});
-    const [errorMessage, setErrorMessage] = useState('');
+import { Navbar, Products, Cart, Checkout } from './components';
+import { commerce } from './lib/commerce';
 
+const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const fetchProducts=async()=>{
-        const {data}=await commerce.products.list();
-        // console.log(data);
-        setProducts(data); 
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout =  (checkoutTokenId, newOrder) => {
+    try {
+      // const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      // setOrder(incomingOrder);
+      // console.log('yes');
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
     }
+  };
 
-    const fetchCart=async()=>{
-        setCart(await commerce.cart.retrieve());
-    }
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
 
-    const handleAddToCart=async(productId,qunatity)=>{
-        const item=await commerce.cart.add(productId,qunatity);
-        setCart(item.cart);
-    }
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
-    const handleUpdateCartQty = async (lineItemId, quantity) => {
-        const response = await commerce.cart.update(lineItemId, { quantity });
-    
-        setCart(response.cart);
-      };
-    
-      const handleRemoveFromCart = async (lineItemId) => {
-        const response = await commerce.cart.remove(lineItemId);
-    
-        setCart(response.cart);
-      };
-    
-      const handleEmptyCart = async () => {
-        const response = await commerce.cart.empty();
-    
-        setCart(response.cart);
-      };
+  return (
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
-      const refreshCart = async () => {
-        const newCart = await commerce.cart.refresh();
-    
-        setCart(newCart);
-      };
-
-      const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
-        try {
-          const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
-    
-          setOrder(incomingOrder);
-    
-          refreshCart();
-        } catch (error) {
-          setErrorMessage(error.data.error.message);
-        }
-      };
-    
-
-    useEffect(()=>{
-        fetchProducts();
-        fetchCart();
-    },[])
-
-    // console.log(products);
-    // console.log(cart);
-    
-    return (
-        <Router>
-            <div>
-                <Navbar totalItems={cart.total_items}></Navbar>
-                <Switch>
-                    <Route exact path="/">
-                        <Products products={products} onAddToCart={handleAddToCart}></Products>
-                    </Route>
-                    <Route exact path="/cart">
-                        <Cart 
-                            cart={cart}
-                            onUpdateCartQty={handleUpdateCartQty}
-                            onRemoveFromCart={handleRemoveFromCart}
-                            onEmptyCart={handleEmptyCart}
-                        />  
-                    </Route>
-                    <Route exact path="/checkout">
-                        <Checkout 
-                            cart={cart} 
-                            order={order} 
-                            onCaptureCheckout={handleCaptureCheckout} 
-                            error={errorMessage}
-                        />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-        
-    )
-}
-
-export default App
+export default App;
